b0c41c061fc34b139a7ce5669e461dd5eb9c83a3
kimet2 <joaquimorteu1@gmail.com>
2022-01-13 12:48:00 +0100

    m

--- PHP_MVC/Models/usuari.php
+++ PHP_MVC/Models/usuari.php
@@ -25,7 +25,7 @@
 
         public function modificar(){
                 $conexion = new database();
-                $sql = "UPDATE usuari SET correu = '$this->correu', telefon = '$this->telefon' WHERE codi = '$this->codi'";
+                $sql = "UPDATE usuari SET nom = '$this->nom', contrasenya = '$this->contrasenya', correu = '$this->correu', adreça = '$this->adreça', dni = '$this->dni', telefon = '$this->telefon', num_tarjeta = '$this->num_tarjeta' WHERE codi = '$this->codi'";
                 $a = $conexion->connect();
                 $a->query($sql);
                 $a->close();
